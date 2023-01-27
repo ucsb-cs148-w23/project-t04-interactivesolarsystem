26ba22b20210761dfe440b54a25274ee66c6edc5
Elizabeth Armstrong <72828201+lzbtharmstrong@users.noreply.github.com>
2023-01-26 16:09:15 -0800

    Create sprintPlanning01.m

---
+++ team/sprint01/sprintPlanning01.m
@@ -0,0 +1,57 @@
+# Sprint Planning meeting
+
+There were 2 meeting that went into sprint planning, 
+
+## 01/21
+Meeting Time: 2:38 PM 
+Meeting Type: Sprint Meeting 
+ 
+Attendance: Ethan Meade [x], Elizabeth Armstrong [x], Aashay Parab [x], ---- 
+ 
+ 
+Aashay: --- 
+ 
+Ethan: --- 
+ 
+Elizabeth: --- 
+ 
+Partial sprint meeting, we’ll get everything settled when we connect with everyone else.  
+ 
+First order of business... we’re using WebGL most likely. Don’t necessarily have to use Unity, but 
+have the option using WebGL.  
+ 
+There is a similar project found elsewhere, built with Vue.js (https://github.com/vuejs/core) for 
+the framework and three.js (https://github.com/mrdoob/three.js) for 3D models.  
+ 
+Had difficulty finding Unity libraries that would help; consult with mentors.  
+  Unity Physics libraries that we saw seem more tailored to classic games on a planet, 
+rather than planets interacting (gravity is just down).  
+ 
+ 
+We decided to get the groundwork set for Tuesday, and then check-in with mentors to make 
+sure everything is being done correctly. 
+ 
+If we have the time we should branch out and try alternatives, but we need to keep the MVP in 
+mind.  
+ 
+Reconnect with group and find a way forwards; we’ll try to investigate some of the possible 
+technologies before Tuesday. In the meantime, we made starter User Stories / Issues which 
+we’ll also refine on Tuesday. 
+
+## 01/24
+Meeting Time: 3:30 pm
+Meeting Type: Spring Meeting
+
+Attendance: Ethan Meade [x], Elizabeth Armstrong [x], Aashay Parab [x], Dingfan Zheng [x], Mason Ma [x], Richard Yand [x]
+
+Richard: Found a site called itch.io that we could publically publish our Unity app on, using this we would have no need to develop our own web app
+and we can focus soley on developing the Unity app 
+
+This has narrowed down our technology search to just developing in Unity
+
+We discussed what we want to have done before the end of the sprint (Friday 01/27). We want to have a base unity app in github that can show a static solar
+system with the sun and other planet objects. 
+
+Ethan: found a template for planet graphics that includes all the planets that could be useful for the visual side of our app
+
+Created a Story based on this goal for the end of the week and made sub-issues to be completed.
