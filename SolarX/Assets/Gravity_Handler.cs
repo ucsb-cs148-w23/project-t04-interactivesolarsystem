5f7e710173848f037a92fae54baba13258fec4ff
Ethan Meade <ethanmeade@ucsb.edu>
2023-02-06 12:59:26 -0800

    EM - Experimentation with scale

--- SolarX/Assets/Gravity_Handler.cs
+++ SolarX/Assets/Gravity_Handler.cs
@@ -8,7 +8,16 @@ public class Gravity_Handler : MonoBehaviour
     // We assume that 1 unit of Mass in Unity is equal to
     // 1 * (10^24) Kilograms (standard when describing celestial bodies)
 
-    // Gravitational Constant is
+    // Gravitational Constant is 6.67 × 10^-11 (Newtons * m^2)/(kg^2)
+    // We can make a new gravitational constant with different units by solving
+    // a change of units problem, where we convert m (meters) to AU and kg (kilograms) to
+    // Earth masses (or some other large mass)
+
+    // Each Unity distance unit is measured to about 0.2 AU
+
+    //const float G = 0.00000000006674f;
+    const float G = 6.674f; // incorrect value, but other value caused things to barely move
+    //static readonly float G = 1.063f*Mathf.Pow(10,17); // Assumes distance is AUs and mass is in Earths
 
     public static List<Gravity_Handler> Gravity_Handlers;
 
@@ -59,7 +68,10 @@ public class Gravity_Handler : MonoBehaviour
             
         }
 
-        float forceMagnitude = (rb.mass * rb_to_attract.mass) / Mathf.Pow(distance, 2);
+        // I've heard that there is a way to optimize this implementation that I haven't applied for now
+        // essentially, dividing by Mathf.Pow() is inefficient compared to another datatype
+        // that would automatically handle it
+        float forceMagnitude = G * (rb.mass * rb_to_attract.mass) / Mathf.Pow(distance, 2);
         Vector3 force = direction.normalized * forceMagnitude;
 
         rb_to_attract.AddForce(force);
