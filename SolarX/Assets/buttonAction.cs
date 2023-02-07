b1957a3facd99ecf5e8e61592beb98b9882d754b
Mason Ma <zhanqingma@ucsb.edu>
2023-02-06 22:33:48 -0800

    add button to Earth

---
+++ SolarX/Assets/buttonAction.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonAction : MonoBehaviour
+{
+    public void buttonMethod()
+    {
+        Debug.Log("11111");
+    }
+}
