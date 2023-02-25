25d17e95c70820c7ce1831ac9f66d2eb68d1adf9
ruiqiRichard <ruiqiyang.richard@gmail.com>
2023-02-24 21:53:02 -0800

    RY-solve duplicate codes

--- SolarX/Assets/Scripts/pauseResume.cs
+++ SolarX/Assets/Scripts/pauseResume.cs
@@ -6,24 +6,17 @@ using UnityEngine.UI;
 public class pauseResume : MonoBehaviour
 {
     public bool isPaused = false;
-    readonly float G = 100f;
 
     public Image buttonIcon;
     public Sprite pauseButton;
     public Sprite resumeButton;
     public GameObject Sun;
     GameObject[] celestials;
-    //public Vector3 v;
-    //public Vector3 av;
-    //public Vector3 gravity;
 
     public void Start()
     {
         Sun = GameObject.Find("Sun Sphere");
-        //v = earth.GetComponent<Rigidbody>().velocity;
-        //av = earth.GetComponent<Rigidbody>().angularVelocity;
         celestials = GameObject.FindGameObjectsWithTag("Celestials");
-        //gravity = earth.GetComponent<Rigidbody>()
     }
     
 
@@ -35,7 +28,7 @@ public class pauseResume : MonoBehaviour
         {
             foreach (GameObject planet in celestials)
             {
-                planet.GetComponent<zizhuan>().enabled = false;
+                planet.GetComponent<SelfRotation>().enabled = false;
                 if (!planet.Equals(Sun))
                 {
                     
@@ -43,42 +36,41 @@ public class pauseResume : MonoBehaviour
                     planet.GetComponent<Rigidbody>().velocity = Vector3.zero;
                 }
                 
+                
             }
-            //earth.GetComponent<zizhuan>().enabled = false;
-            //earth.GetComponent<Rigidbody>().isKinematic = true;
             buttonIcon.sprite = resumeButton;
             
         } else
         {
             foreach (GameObject planet in celestials)
             {
-                planet.GetComponent<zizhuan>().enabled = true;
+                planet.GetComponent<SelfRotation>().enabled = true;
                 if (!planet.Equals(Sun))
                 {
                     planet.GetComponent<Rigidbody>().isKinematic = false;
                 }
             }
-            InitialVelocity();
+            SolarSystem.InitialVelocity(celestials);
             buttonIcon.sprite = pauseButton;
         }
     }
 
-    void InitialVelocity()
-    {
-        foreach (GameObject a in celestials)
-        {
-            foreach (GameObject b in celestials)
-            {
-                if (!a.Equals(b))
-                {
-                    float m2 = b.GetComponent<Rigidbody>().mass;
-                    float r = Vector3.Distance(a.transform.position, b.transform.position);
-                    a.transform.LookAt(b.transform);
+    //void InitialVelocity()
+    //{
+    //    foreach (GameObject a in celestials)
+    //    {
+    //        foreach (GameObject b in celestials)
+    //        {
+    //            if (!a.Equals(b))
+    //            {
+    //                float m2 = b.GetComponent<Rigidbody>().mass;
+    //                float r = Vector3.Distance(a.transform.position, b.transform.position);
+    //                a.transform.LookAt(b.transform);
 
-                    a.GetComponent<Rigidbody>().velocity += a.transform.right * Mathf.Sqrt((G * m2) / r);
-                }
-            }
-        }
-    }
+    //                a.GetComponent<Rigidbody>().velocity += a.transform.right * Mathf.Sqrt((G * m2) / r);
+    //            }
+    //        }
+    //    }
+    //}
 
 }
