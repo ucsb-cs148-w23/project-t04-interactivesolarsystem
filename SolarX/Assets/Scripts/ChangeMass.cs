00880dc9129c8549b42d0994b7e5e060410735b5
Elizabeth Armstrong <esarmstrong@ucsb.edu>
2023-03-07 14:52:26 -0800

    Made it not error

--- SolarX/Assets/Scripts/ChangeMass.cs
+++ SolarX/Assets/Scripts/ChangeMass.cs
@@ -1,3 +1,5 @@
+//#include <stdio.h>
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -20,17 +22,19 @@ public class ChangeMass : MonoBehaviour
     //Mass is in the UnityUnits we are unsing
     void changeMassUnityUnits(Rigidbody rb, float mass){
         //Mass can't be zero or lower and is capped at 1,000,000 just to keep program normal
+        /*
         if(mass <= 0 || mass > 1000000){
             Debug.Log("Mass input: " + mass + " is out of bounds");
             return;
         }
         rb.mass = mass;
+        */
     }
 
     //Change the mass of a planet using regular units
     // Units of mass: 10^23 kgs
     void changeMassKgs(Rigidbody rb, float mass){
         //Divide value by mass of the earth because that is what 1 unit is in our unity unit scaling.
-        changeMassUnityUnits(rb, mass/(59.7219))
+        //changeMassUnityUnits(rb, mass/((float)59.7219));
     }
 }
