8a48ff562789e00fa6e4ee7acf40e82b83562564
Aashay Parab <aashay_parab@ucsb.edu>
2023-03-10 09:25:23 -0800

    Stashed changes

--- SolarX/Assets/Scripts/mouselook.cs
+++ SolarX/Assets/Scripts/mouselook.cs
@@ -1,50 +1,84 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+
+
+// public class mouselook : MonoBehaviour
+// {
+//     //sensitivity => ������
+//     public float MouseSensitivity = 100f;
+
+//     public Transform PlayerBody;
+
+//     private float XRotation = 0f;
+
+//     Quaternion localspace;
+//     float MouseX;
+//     float MouseY;
+
+
+//     // Start is called before the first frame update
+//     void Awake()
+//     {
+//         localspace = gameObject.transform.rotation;
+//         MouseX = localspace.x;
+//         MouseY = localspace.y;
+   
+        
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         if(DisconnectCam.instance.getDisconnected()){
+//             return;
+//         }
+        
+//         MouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
+//          MouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
+
+//         XRotation -= MouseY;
+//         XRotation = Mathf.Clamp(XRotation, -90f, 90f);
+
+//         transform.localRotation =  Quaternion.Euler(XRotation, 0f, 0f);
+
+//         PlayerBody.Rotate(Vector3.up * MouseX);
+
+//     }
+// }
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class MouseLook: MonoBehaviour{
+    [Tooltip("A multiplier to the input. Describes the maximum speed in degrees / second. To flip vertical rotation, set Y to a negative value")]
+    [SerializeField] private Vector2 sensitivity; 
 
+    private Vector2 rotation; 
 
-public class mouselook : MonoBehaviour
-{
-    //sensitivity => ������
-    public float MouseSensitivity = 100f;
-
-    public Transform PlayerBody;
-
-    private float XRotation = 0f;
-
-    Quaternion localspace;
-    float MouseX;
-    float MouseY;
-
-
-    // Start is called before the first frame update
-    void Awake()
+    private Vector2 GetInput()
     {
-        localspace = gameObject.transform.rotation;
-        MouseX = localspace.x;
-        MouseY = localspace.y;
-   
+        Vector2 input = new Vector2(
+            Input.GetAxis("Mouse X"), 
+            Input.GetAxis("Mouse Y")
+        );
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(DisconnectCam.instance.getDisconnected()){
-            return;
-        }
-        
-        MouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
-         MouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
-
-        XRotation -= MouseY;
-        XRotation = Mathf.Clamp(XRotation, -90f, 90f);
-
-        transform.localRotation =  Quaternion.Euler(XRotation, 0f, 0f);
-
-        PlayerBody.Rotate(Vector3.up * MouseX);
-
+        return input; 
     }
 }
 
+private void Update()
+{
+    //The wanted velocity is the current input scaled by the sensitivity 
+    //This is also the maximum velocity 
+
+    Vector2 wantedVelocity = GetInput * sensitivity; 
+    
+    //New rotation 
+    rotation += watnedVelocity * Time.deltaTime; 
+
+    //Convert the rotation to euler angles 
+    transform.localEulerAngles = new Vector3(rotation.y, rotation.x, 0); 
+}
