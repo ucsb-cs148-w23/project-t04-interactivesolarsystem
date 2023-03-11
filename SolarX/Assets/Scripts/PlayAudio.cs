160d27a27f39162fb7f236f80a6031d3f145d146
Mason Ma <zhanqingma@ucsb.edu>
2023-03-10 16:47:12 -0800

    fixed errors

--- SolarX/Assets/Scripts/PlayAudio.cs
+++ SolarX/Assets/Scripts/PlayAudio.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class PlayAudio : MonoBehaviour
 {
-    public GameObject xinxi;
     [SerializeField, TextArea] private string theText;
     private void OnMouseDown()
     {
         UIcontroller.instance.Open(theText);
-        xinxi.SetActive(false);
     }
 
 
